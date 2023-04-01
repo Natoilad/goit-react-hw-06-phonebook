3a8bacf8b9b78c0b2bc3d0ed6dcd18b6a3cf6faf
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-04-01 03:39:51 +0300

    persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,46 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit';
-// import { storage } from 'redux-persist/lib/storage';
-// import {
-//   persistStore,
-//   persistReducer,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import { sliceContact } from './sliceContact';
 import { sliceFilter } from './sliceFilter';
 
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-//   whiteList: ['contacts'],
-// };
+const persistConfig = {
+  key: 'root',
+  storage,
+  whiteList: ['contacts'],
+};
 
-// const rootReducer = createReducer({
-//   contacts: sliceContact.reducer,
-//   filter: sliceFilter.reducer,
-// });
-
-// const persistReducerContacts = persistReducer(persistConfig, rootReducer);
-
-export const store = configureStore({
-  reducer: {
-    contacts: sliceContact.reducer,
-    filter: sliceFilter.reducer,
-  },
-  //   reducer: persistReducerContacts,
-  //   middleware: getDefaultMiddleware =>
-  // getDefaultMiddleware({
-  //   serializableCheck: {
-  //     ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-  //   },
-  // }),
+const rootReducer = combineReducers({
+  contacts: sliceContact.reducer,
+  filter: sliceFilter.reducer,
 });
 
-// export const persistR = persistStore(store);
+const persistReducerContacts = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistReducerContacts,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistR = persistStore(store);
 
 // // Infer the `RootState` and `AppDispatch` types from the store itself
 // export type RootState = ReturnType<typeof store.getState>;
